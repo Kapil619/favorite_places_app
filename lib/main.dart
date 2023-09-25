02522f10c0ce94791fd8cd822024e2358b7805ea
kapil619 <kapilbadokar321@gmail.com>
2023-09-25 19:18:47 +0530

    Added a places screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -27,7 +28,7 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-      const MyApp(),
+    const MyApp(),
   );
 }
 
@@ -39,7 +40,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Great Places',
       theme: theme,
-      home: ...,
+      home: const PlacesScreen(),
     );
   }
 }
---
+++ lib/screens/places.dart
@@ -0,0 +1,22 @@
+import 'package:favorite_places/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: const PlacesList(places: []),
+    );
+  }
+}
