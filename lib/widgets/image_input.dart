1314daebcd52f6a5db691c8c009cdc02eee01700
kapil619 <kapilbadokar321@gmail.com>
2023-09-29 22:26:07 +0530

    using camera to take pictures

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -10,23 +12,48 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  File? _selectedImage;
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border:
-            Border.all(width: 1, color: Theme.of(context).colorScheme.primary),
-      ),
-      height: 250,
-      width: double.infinity,
-      alignment: Alignment.center,
-      child: TextButton.icon(
-        onPressed: _takePicture,
-        icon: const Icon(Icons.camera),
-        label: const Text('Take picture'),
-      ),
+    Widget content = TextButton.icon(
+      onPressed: _takePicture,
+      icon: const Icon(Icons.camera),
+      label: const Text('Take picture'),
     );
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+    return Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+              width: 1, color: Theme.of(context).colorScheme.primary),
+        ),
+        height: 250,
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: content);
   }
 }
