f20143127c975f82a59e5924bd9b4db533098227
kapil619 <kapilbadokar321@gmail.com>
2023-09-29 22:05:33 +0530

    created widget for adding image

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -46,6 +47,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               style:
                   TextStyle(color: Theme.of(context).colorScheme.onBackground),
             ),
+            const SizedBox(height: 16),
+            ImageInput(),
             const SizedBox(
               height: 16,
             ),
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() {
+    return _ImageInputState();
+  }
+}
+
+class _ImageInputState extends State<ImageInput> {
+  void _takePicture() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border:
+            Border.all(width: 1, color: Theme.of(context).colorScheme.primary),
+      ),
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: TextButton.icon(
+        onPressed: _takePicture,
+        icon: const Icon(Icons.camera),
+        label: const Text('Take picture'),
+      ),
+    );
+  }
+}
